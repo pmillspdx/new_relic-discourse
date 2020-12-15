8a28484e0f43b1330e35684171e9bfbf4ba2cb04
Phil Mills <pmillspdx@users.noreply.github.com>
2020-12-15 11:24:44 -0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: New Relic RPM
 # about: Report performance data to http://www.newrelic.com/
-# version: 6.13.0
+# version: 6.14.0
 # authors: Sam Saffron, David Celis, Phil Mills
 
 ENV['NRCONFIG'] = File.expand_path('../newrelic.yml', __FILE__)
