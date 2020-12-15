f75d014a507a973a4a056d0837f1e7b85fb4bd89
Phil Mills <pmillspdx@users.noreply.github.com>
2020-12-15 08:31:00 -0800

    Update Ruby Agent to 6.14.0

--- plugin.rb
+++ plugin.rb
@@ -4,7 +4,7 @@
 # authors: Sam Saffron, David Celis, Phil Mills
 
 ENV['NRCONFIG'] = File.expand_path('../newrelic.yml', __FILE__)
-gem 'newrelic_rpm', ENV['NEW_RELIC_AGENT_VERSION'] || '6.13.0'
+gem 'newrelic_rpm', ENV['NEW_RELIC_AGENT_VERSION'] || '6.14.0'
 
 NewRelic::Control.instance.init_plugin config: Rails.configuration
 
